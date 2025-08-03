916eb3883f24350db5767bc308d93711c4ef0a7b
Ana Cristina <anacrispee@edu.univali.br>
2025-08-02 21:28:57 -0300

    fix: remove dummy list

--- domain/src/main/java/com/example/domain/model/BookModel.kt
+++ domain/src/main/java/com/example/domain/model/BookModel.kt
@@ -20,30 +20,3 @@ data class InformationModel(
     val pageCount: Int? = null,
     val currentPage: Int? = null
 ) : Parcelable
-
-val dummyBooksList = listOf(
-    BookModel(
-        information = InformationModel(
-            title = "Jogos Vorazes",
-            subtitle = "Vol. 1",
-            authors = listOf("Suzanne Collins"),
-            cover = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1586722975i/2767052.jpg"
-        )
-    ),
-    BookModel(
-        information = InformationModel(
-            title = "Harry Potter e a Ordem da Fênix",
-            subtitle = "Vol. 5",
-            authors = listOf("J.K. Rowling"),
-            cover = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1546910265i/2.jpg"
-        )
-    ),
-    BookModel(
-        information = InformationModel(
-            title = "Crepúsculo",
-            subtitle = "Saga Crepúsculo",
-            authors = listOf("Stephenie Meyer"),
-            cover = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1700522826i/41865.jpg"
-        )
-    )
-)
